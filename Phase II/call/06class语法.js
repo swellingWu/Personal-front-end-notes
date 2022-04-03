f9521977561378981634d414fd93dfd75e780535
nep <3368939477@qq.com>
2022-04-03 21:04:24 +0800

    开始更新实例

--- Phase II/call/06class语法.js
+++ Phase II/call/06class语法.js
@@ -1,10 +1,22 @@
-class Uaer {
-    constructor(name) {
-        this.name = name;
-    }
-    sayhi() {
-        alert(this.name);
-    }
+// class Uaer {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     sayhi() {
+//         alert(this.name);
+//     }
+// }
+// let user = new User();//=》 new() 调用 constructor 
+// user.sayhi();
+
+
+// 使用纯函数重写
+function User(name) {
+    this.name = name;
 }
-let user = new User();//=》 new() 调用 constructor 
+User.prototype.sayhi = function sayhi() {
+    alert(this.name);
+}
+
+let user = new User("John");
 user.sayhi();
---
+++ javaScript俄罗斯方块/app.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScoreDisplay = document.querySelector('#score');
+    const SrartBtn = document.querySelector('#start-button');
+    const width = 10;
+
+    //- 旋转转换
+    const lTetromino = [
+        [1,width+1,width*2+1,2],
+        [width,width+1,width+2,width*2+1],
+        [1,width+1,width*2+1,width*2],
+        [width,width*2,width*2+1,width*2+2],
+    ];
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+    ];
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1],
+    ];
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+    ];
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+    ];
+
+    const theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino];
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+
+    let random = Math.floor(Math.random()*theTetrominoes.length);
+
+    let current = theTetrominoes[random][currentRotation];
+
+    //- 绘制第一个旋转
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition+index].classList.add('tetromino');
+        })
+    }
+
+    //- 清除方块
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino');
+        })
+    }
+})
