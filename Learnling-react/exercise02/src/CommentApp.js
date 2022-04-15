e77dbf65a930e4e086fd2e1cec099998f87a34c4
nep <3368939477@qq.com>
2022-04-15 15:33:19 +0800

    react_1

---
+++ Learnling-react/exercise02/src/CommentApp.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import CommentInput from './CommentInput'
+import CommentList from './CommentList'
+
+class CommentApp extends Component {
+    handleSubmitComment = (comment) => {console.log(comment)}
+
+    render () {
+        return (
+            <div className="wrapper">
+                <CommentInput 
+                  onSubmit={this.handleSubmitComment.bind(this)}
+                />
+                <CommentList />
+            </div>
+        )
+    }
+}
+
+export default CommentApp
---
+++ Learnling-react/exercise02/src/CommentList.js
@@ -0,0 +1,11 @@
+import React,{ Component } from 'react'
+
+class CommentList extends Component {
+    render() {
+        return (
+            <div>CommList</div>
+        )
+    }
+}
+
+export default CommentList
